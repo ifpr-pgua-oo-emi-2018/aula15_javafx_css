1a47f49eb4e105ade35f12a49b5eed9c1749b12c
Hugo Perlin <hugo.perlin@ifpr.edu.br>
2019-06-10 09:51:38 -0300

    códigos da aula

--- JavafxCss/src/sample/control/Controller.java
+++ JavafxCss/src/sample/control/Controller.java
@@ -9,11 +9,14 @@ import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.web.WebView;
 import javafx.util.Callback;
 
 import java.awt.*;
 import java.net.URI;
+import java.util.function.Predicate;
 
 
 //Para maiores detalhes das propriedades css dos componentes
@@ -38,6 +41,9 @@ public class Controller {
     private TextField tfNome;
 
     @FXML
+    private TextField tfBusca;
+
+    @FXML
     private CheckBox chbAjuda;
 
     @FXML
@@ -50,6 +56,9 @@ public class Controller {
     private void initialize(){
         lista = FXCollections.observableArrayList();
         ltvItems.setItems(lista);
+
+        tfNome.setStyle("-fx-text-fill: green");
+
 
 
         ltvItems.setCellFactory(new Callback<ListView<String>, ListCell<String>>() {
@@ -86,17 +95,6 @@ public class Controller {
             }
         });
 
-        chbAjuda.selectedProperty().addListener(new ChangeListener<Boolean>() {
-            @Override
-            public void changed(ObservableValue<? extends Boolean> observableValue, Boolean aBoolean, Boolean t1) {
-
-                if(t1){
-                    Alert a = new Alert(Alert.AlertType.INFORMATION,"Acesse: \n  ");
-                    a.showAndWait();
-                }
-            }
-        });
-
     }
 
 
@@ -116,6 +114,46 @@ public class Controller {
 
     @FXML
     private void addNome(){
+        System.out.println(tfNome.getText());
         lista.add(tfNome.getText());
     }
+
+    @FXML
+    private void acaoTecla(KeyEvent e){
+
+        System.out.println(e.getCode());
+
+        if(e.isControlDown() && e.getCode() == KeyCode.Z){
+            ltvItems.setItems(lista);
+            tfBusca.clear();
+        }
+
+
+
+        String filtro = tfBusca.getText()+e.getText();
+
+        if(filtro.length()>=3){
+            ltvItems.setItems(filtra(filtro));
+        }
+
+
+    }
+
+    private ObservableList<String> filtra(String filtro){
+        ObservableList<String> temp = FXCollections.observableArrayList();
+
+        temp.addAll(lista.filtered(new Predicate<String>() {
+            @Override
+            public boolean test(String s) {
+                return s.startsWith(filtro);
+
+            }
+        }));
+
+        return temp;
+
+    }
+
+
+
 }
